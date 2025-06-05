f47ed1ec2f0b294e1a083fe36b6efdeb757bc3d5
DJENJNY <ethan.jon32@gmail.com>
2025-06-05 12:04:47 -0500

    Generated K8s Config files for Authentication

--- client.sh
+++ client.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 
-mkdir /var/lib/kubelet/
+mkdir -p /var/lib/{kube-proxy,kubelet}
 
 cp /vagrant/ca.crt /var/lib/kubelet/ca.crt
 
 cp /vagrant/$(hostname).crt /var/lib/kubelet/kubelet.crt
 
 cp /vagrant/$(hostname).key /var/lib/kubelet/kubelet.key
+
+cp /vagrant/kube-proxy.kubeconfig /var/lib/kube-proxy/kubeconfig 
+
+cp /vagrant/$(hostname).kubeconfig /var/lib/kubelet/kubeconfig
+
+
+
+
